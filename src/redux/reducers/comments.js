1eee5b8961e39edc7aafcc6a1ef94a8700618921
Jonathan Pollock <jon.pollock@me.com>
2017-08-08 22:38:06 -0400

    wip

--- src/redux/reducers/comments.js
+++ src/redux/reducers/comments.js
@@ -12,11 +12,15 @@ function comments(state=[], action) {
 			console.log(typeof state);
 			console.log(state);
 			console.log(`-------> Post Id ${action.postId}`);
-			const copiedState = Array.from(state)
-			return [
+			const copiedState = Object.assign({}, state);
+			console.log("--------------");
+			console.log(typeof copiedState);
+			console.log(copiedState);
+			console.log("--------------");
+			return {
 					...copiedState.slice(0, action.postId), // before what we are updating
 					...copiedState.slice(action.postId + 1) // after the deleted one
-			]
+			}
 
 		default:
 			return state;
